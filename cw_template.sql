9c61c47295e7edfbf29076c354d4363af59b538b
Alexander Smolowitz <alexander.smolowitz@city.ac.uk>
2024-11-26 15:04:10 +0000

    Added Content

---
+++ cw_template.sql
@@ -0,0 +1,105 @@
+/*
+Student name: Alexander Smolowitz
+Student ID: 230065722
+*/
+-- Show DB info and Table info, dropping incase already exists --
+DROP DATABASE IF EXISTS CourseWork;
+CREATE DATABASE CourseWork;
+USE CourseWork;
+/* SECTION 1 - CREATE TABLE STATEMENTS */
+CREATE TABLE User ();
+
+
+/* SECTION 2 - INSERT STATEMENTS */
+
+
+                     
+/* SECTION 3 - UPDATE STATEMENTS - The queries must be explained in natural (English) language first, and then followed up by respective statements */
+
+/*
+1)
+
+*/
+
+/*
+2)
+
+*/
+
+
+/* SECTION 4 - SELECT STATEMENTS - The queries must be explained in natural (English) language first, and then followed up by respective SELECTs*/
+
+
+/* 
+1)  
+
+*/
+
+
+
+/* 
+2)  
+
+*/
+
+
+
+
+/* 
+3)  
+
+*/
+
+
+
+/* 
+4)  
+
+*/
+
+
+
+
+/* 
+5)  
+
+*/
+
+
+
+/* 
+6)  
+
+*/
+
+
+/* 
+7)  
+
+*/
+
+
+
+/* 
+8)  
+
+*/
+
+
+
+/* SECTION 5 - DELETE ROWS - The queries must be explained in natural (English) language first, and then followed up by respective statements */
+
+/*
+1)
+
+*/
+
+/*
+2)
+
+*/
+
+
+/* SECTION 6 - DROP TABLES */
+
+
